c0543177b3d9a6c09aa95a4abf43232e31d5e120
Jesse <dis@iqip.com>
2026-08-24 18:14:24 +0200

    added test script for windows

---
+++ scenes/lvl_0/tests/test_map_gen.ps1
@@ -0,0 +1,2 @@
+﻿Set-Location "$PSScriptRoot\..\..\.."
+& "C:\Program Files (x86)\Steam\steamapps\common\Godot Engine\godot.windows.opt.tools.64.exe" --headless --path . -s scenes/lvl_0/tests/test_map_gen.gd
