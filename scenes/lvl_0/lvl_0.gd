590801ff131a25923bb567ec008942399956a5d9
jesse <jessevandis@gmail.com>
2026-08-24 22:01:52 +0200

    wip infinite level rework...

--- scenes/lvl_0/lvl_0.gd
+++ scenes/lvl_0/lvl_0.gd
@@ -9,77 +9,108 @@ const ceiling_light_scene = preload("res://scenes/lvl_0/part_1x1_ceiling_light.t
 
 const tile_size = 1.0
 
+var map: MapGenerator.Section
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Clear existing map children
 	#for child in map_node.get_children():
 	#	child.queue_free()
 	
-	var image = generate_level()
+	generate_level()
 		
 	# Find a spawn point (white pixel)
-	var spawn_pos = _find_spawn_point(image)
+	var spawn_pos = _find_spawn_point()
 	
 	$Player.transform.origin = Vector3(spawn_pos.x * tile_size, 0.0, spawn_pos.y * tile_size)
 	# Also reset rotation to avoid looking at the floor or something
 	$Player.rotation = Vector3.ZERO
 
-func generate_level() -> Image:
-	const width = 64
-	const height = 64
-	
-	var image = MapGenerator.new().generate_map_image(width, height, 1234) #randi())
-	
-	for x in range(width):
-		for z in range(height):
-			var is_wall = MapGenerator.is_wall(image, x, z);
-			var is_light = MapGenerator.is_light(image, x, z);
-			
-			var pos = Vector3(x * tile_size, 0, z * tile_size)
-			
-			# Floor and Ceiling for every pixel
-			var floor_inst = floor_scene.instantiate()
-			map_node.add_child(floor_inst)
-			floor_inst.transform.origin = pos
-			
-			var ceiling_inst = ceiling_scene.instantiate()
-			map_node.add_child(ceiling_inst)
-			ceiling_inst.transform.origin = pos
-			
-			if is_light:
+func generate_level() -> void:
+	const radius = 64
+		
+	var place_tile: Callable = func(x: int, y: int, tile_pos: Vector3, tile_type: MapGenerator.TileType) -> void:
+		# ALWAYS add a floor and ceiling
+		var floor_inst   = floor_scene.instantiate()
+		var ceiling_inst = ceiling_scene.instantiate()
+		map_node.add_child(floor_inst)
+		map_node.add_child(ceiling_inst)
+		floor_inst.transform.origin = tile_pos
+		ceiling_inst.transform.origin = tile_pos
+
+		# now what to put there...
+		match tile_type:
+			MapGenerator.TileType.EMPTY:
+				# do nothing
+				return
+			MapGenerator.TileType.CEILING_LIGHT:
 				var ceiling_light_inst = ceiling_light_scene.instantiate()
 				map_node.add_child(ceiling_light_inst)
-				ceiling_light_inst.transform.origin = pos
+				ceiling_light_inst.transform.origin = tile_pos
 				
-			
-			if is_wall: # Check neighbors for walls
-				_check_and_add_wall(image, x, z, x + 1, z, -PI/2, Vector3(tile_size/2, 0, 0)) # Right
-				_check_and_add_wall(image, x, z, x - 1, z, PI/2, Vector3(-tile_size/2, 0, 0)) # Left
-				_check_and_add_wall(image, x, z, x, z + 1, PI, Vector3(0, 0, tile_size/2)) # Back
-				_check_and_add_wall(image, x, z, x, z - 1, 0, Vector3(0, 0, -tile_size/2)) # Front
-	
-	return image
+			MapGenerator.TileType.WALL:
+				pass
 
-func _check_and_add_wall(image: Image, x: int, z: int, nx: int, nz: int, angle: float, offset: Vector3):
-	var width: int  = image.get_width()
-	var height: int = image.get_height()
-	
-	var is_neighbor_empty: bool = true
-	if nx >= 0 and nx < width and nz >= 0 and nz < height:
-		if MapGenerator.is_wall(image, nx, nz):
-			is_neighbor_empty = false
-	
-	if is_neighbor_empty:
-		var wall_inst: Node3D = wall_scene.instantiate()
-		map_node.add_child(wall_inst)
-		wall_inst.transform.origin = Vector3(x * tile_size, 0, z * tile_size) + offset
-		wall_inst.rotate_y(angle)
+	var generator = MapGenerator.new();
 
-func _find_spawn_point(image: Image) -> Vector2i:
-	for x in range(image.get_width()):
-		for z in range(image.get_height()):
-			if ! MapGenerator.is_wall(image, x, z):
-				return Vector2i(x, z)
+	map = generator.generate_map(-radius, -radius, radius, radius)	
+	for y in range(map.y, map.y + map.h):
+		for x in range(map.x, map.x + map.w):
+			place_tile.call(x, y, Vector3(x,0,y), map.get_tile(x, y));
+
+	
+	#
+	#var image = MapGenerator.new().generate_map_image(width, height, 1234) #randi())
+	#
+	#for x in range(width):
+		#for z in range(height):
+			#var is_wall = MapGenerator.is_wall(image, x, z);
+			#var is_light = MapGenerator.is_light(image, x, z);
+			#
+			#var pos = Vector3(x * tile_size, 0, z * tile_size)
+			#
+			## Floor and Ceiling for every pixel
+			#var floor_inst = floor_scene.instantiate()
+			#map_node.add_child(floor_inst)
+			#floor_inst.transform.origin = pos
+			#
+			#var ceiling_inst = ceiling_scene.instantiate()
+			#map_node.add_child(ceiling_inst)
+			#ceiling_inst.transform.origin = pos
+			#
+			#if is_light:
+				#var ceiling_light_inst = ceiling_light_scene.instantiate()
+				#map_node.add_child(ceiling_light_inst)
+				#ceiling_light_inst.transform.origin = pos
+				#
+			#
+			#if is_wall: # Check neighbors for walls
+				#_check_and_add_wall(image, x, z, x + 1, z, -PI/2, Vector3(tile_size/2, 0, 0)) # Right
+				#_check_and_add_wall(image, x, z, x - 1, z, PI/2, Vector3(-tile_size/2, 0, 0)) # Left
+				#_check_and_add_wall(image, x, z, x, z + 1, PI, Vector3(0, 0, tile_size/2)) # Back
+				#_check_and_add_wall(image, x, z, x, z - 1, 0, Vector3(0, 0, -tile_size/2)) # Front
+	
+
+#func _check_and_add_wall(image: Image, x: int, z: int, nx: int, nz: int, angle: float, offset: Vector3):
+	#var width: int  = image.get_width()
+	#var height: int = image.get_height()
+	#
+	#var is_neighbor_empty: bool = true
+	#if nx >= 0 and nx < width and nz >= 0 and nz < height:
+		#if MapGenerator.is_wall(image, nx, nz):
+			#is_neighbor_empty = false
+	#
+	#if is_neighbor_empty:
+		#var wall_inst: Node3D = wall_scene.instantiate()
+		#map_node.add_child(wall_inst)
+		#wall_inst.transform.origin = Vector3(x * tile_size, 0, z * tile_size) + offset
+		#wall_inst.rotate_y(angle)
+#
+func _find_spawn_point() -> Vector2i:
+	for y in range(map.y, map.y + map.h):
+		for x in range(map.x, map.x + map.w):
+			if map.get_tile(x, y) != MapGenerator.TileType.WALL:
+				return Vector2i(x, y)
 	return Vector2i(1, 1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scenes/lvl_0/map_generator.gd
+++ scenes/lvl_0/map_generator.gd
@@ -9,14 +9,25 @@ enum TileType {
 }
 
 class PlotData:
+	var tile_type: TileType
+	func _init() -> void:
+		pass;
+
+class Section:
 	var x: int
 	var y: int
-	var color: Color
-
-	func _init(_x: int, _y: int, _color: Color) -> void:
-		x = _x
-		y = _y
-		color = _color
+	var w: int
+	var h: int
+	var data: Array[TileType] = []
+	
+	func set_tile(x: int, y: int, tile_type: TileType) -> void:
+		data[x + y * w] = tile_type;
+		
+	func get_tile(x: int, y: int) -> TileType:
+		return data[x + y * w];
+	
+	func _init() -> void:
+		pass;
 
 class Context:
 	var x: int
@@ -29,8 +40,8 @@ class Context:
 		return rng.randf()
 
 func _mapshader_standard(context: Context) -> TileType:
-	if context.random() > 0.5:
-		return TileType.WALL
+	if context.random() > 0.9:
+		return TileType.CEILING_LIGHT
 	return TileType.EMPTY
 
 func to_color(tile_type: TileType) -> Color:
@@ -45,19 +56,34 @@ func to_color(tile_type: TileType) -> Color:
 
 func generate_map_image(width: int, height: int) -> Image:
 	var image = Image.create(width, height, false, Image.FORMAT_RGB8)
+		
+	var map: Section = generate_map(0, 0, width, height);
+	for y in range(0, height):
+		for x in range(0, width):
+			image.set_pixel(x, y, to_color(map.get_tile(x, y)))
 	
-	var cb: Callable = func(context: Context, tile_type: TileType) -> void:
-		image.set_pixel(context.x, context.y, to_color(tile_type))
-	
-	generate_map(0, 0, width, height, cb);
 	return image;
 	
 
-func generate_map(x0: int, y0: int, x1: int, y1: int, cb: Callable) -> void:
+func generate_map(x0: int, y0: int, x1: int, y1: int) -> Section:
+	var retval: Section = Section.new();
+	retval.x = x0;
+	retval.y = y0;
+	retval.w = x1-x0;
+	retval.h = y1-y0;
+	retval.data.resize(retval.w * retval.h);
+	var cb: Callable = func(context: Context, data: PlotData) -> void:
+		retval.set_tile(context.x, context.y, data.tile_type);
+	_handle_shaders(x0, y0, x1, y1, cb);
+	
+	return retval;
+
+func _handle_shaders(x0: int, y0: int, x1: int, y1: int, cb: Callable) -> void:
 	var context: Context = Context.new()
+	var cb_data: PlotData = PlotData.new()
 	for y in range(x0, x1):
 		for x in range(y0, y1):
 			context.x = x;
 			context.y = y;
-			var pixel = _mapshader_standard(context);
-			cb.call(context, pixel);
+			cb_data.tile_type = _mapshader_standard(context);
+			cb.call(context, cb_data);
