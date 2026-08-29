d7c7e5bc4869fe5e5dfc5ae7e194aa2eb926428d
jesse <jessevandis@gmail.com>
2026-08-29 12:06:27 +0200

    level is now infinite. but need to remove the 'old' collision shapes still

--- scenes/lvl_0/lvl_0.gd
+++ scenes/lvl_0/lvl_0.gd
@@ -11,7 +11,7 @@ const wall_scene_i: Resource = preload("res://scenes/lvl_0/part_wall_i.tscn")
 const wall_scene_l: Resource = preload("res://scenes/lvl_0/part_wall_l.tscn")
 const wall_scene_e: Resource = preload("res://scenes/lvl_0/part_wall_end.tscn")
 
-const tile_size: float = 1.0
+const TILE_SIZE: float = 1.0
 const CHUNK_SIZE: int = 64
 const GENERATION_THRESHOLD: float = 15.0
 const REMOVAL_THRESHOLD: float = 200.0
@@ -19,6 +19,7 @@ const REMOVAL_THRESHOLD: float = 200.0
 
 var model_floor: Model = _load_model(floor_scene)
 var model_ceiling: Model = _load_model(ceiling_scene)
+var model_ceiling_light: Model = _load_model(ceiling_light_scene)
 var model_wall_x: Model = _load_model(wall_scene_x)
 var model_wall_t: Model = _load_model(wall_scene_t)
 var model_wall_i: Model = _load_model(wall_scene_i)
@@ -33,10 +34,8 @@ class Model:
 		pass;
 
 class Chunk:
-	var chunk_index_x: int
-	var chunk_index_y: int
-	var global_pos_x: int
-	var global_pos_y: int
+	var chunk_index: Vector2i
+	var global_pos: Vector2
 	var static_body_3d: StaticBody3D
 	var tiles: Node3D
 	var section: MapGenerator.Section
@@ -44,112 +43,97 @@ class Chunk:
 	func _init() -> void:
 		pass;
 
-
-var chunks: Dictionary = {} # Vector2i -> Chunk
+class PlacedTile:
+	# constant
+	var model: Model
+	var angle: float
+	
+	# mutable
+	var collision_shape: CollisionShape3D
+	
+	func _init() -> void:
+		pass;
+	
+var chunks: Dictionary = {}        # Vector2i(chunk index) -> Chunk
+var placed_tiles: Dictionary = {}  # Vector2i(tile index)  -> CollisionTile
 #var rendered_chunks: Dictionary = {} # Vector2i -> Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Initial generation
-	_generate_chunk(Vector2i(0, 0))
+	_get_or_create_chunk(Vector2i(0, 0))
 	
 	# Find a spawn point (white pixel)
 	var spawn_pos: Vector2i = _find_spawn_point()
 	
-	$Player.transform.origin = Vector3(float(spawn_pos.x) * tile_size, 0.0, float(spawn_pos.y) * tile_size)
-	# Also reset rotation to avoid looking at the floor or something
-	$Player.rotation = Vector3.ZERO
+	$Player.transform.origin = Vector3(float(spawn_pos.x) * TILE_SIZE, 0.0, float(spawn_pos.y) * TILE_SIZE)
 
-func _place_wall(parent: Node3D, static_body: StaticBody3D, model: Model, pos: Vector3, angle: float) -> void:
-	_instantiate_model(parent, static_body, model, pos, angle)
-
-#func _move_collision_shapes(source_node: Node, target_body: StaticBody3D) -> void:
-	#for child: Node in source_node.get_children():
-		#if child is CollisionShape3D:
-			#child.reparent(target_body, true)
-		##else:
-		##	_move_collision_shapes(child, target_body)
+func _place_wall(parent: Node3D, static_body: StaticBody3D, model: Model, tile_index: Vector2i, angle: float) -> void:
+	_instantiate_model(parent, static_body, model, tile_index, angle)
 
 func _get_tile_at(x: int, y: int) -> MapGenerator.TileType:
 	var cp: Vector2i = Vector2i(int(floor(float(x) / CHUNK_SIZE)), int(floor(float(y) / CHUNK_SIZE)))
 	if ! chunks.has(cp):
 		return MapGenerator.TileType.WALL
-	var section: MapGenerator.Section = _ensure_chunk_data(cp).section
+	var section: MapGenerator.Section = _get_or_create_chunk(cp).section
 	return section.get_tile(x, y)
 
-#func _generate_common_collision_shapes(chunk: Chunk):
-	#for y in range(chunk.section.y, chunk.section.y + chunk.section.h):
-		#for x in range(chunk.section.x, chunk.section.x + chunk.section.w):
-			## always add floor
-			#var collision_shape := CollisionShape3D.new()
-			#var box_shape := BoxShape3D.new()
-			#box_shape.size = Vector3(1, 1, 1)
-			#collision_shape.shape = box_shape
-			#collision_shape.position = Vector3(x, -0.5, y)
-			#chunk.static_body_3d.add_child(collision_shape)
-			#
-	#pass
-
-func _ensure_chunk_data(cp: Vector2i) -> Chunk:
-	if chunks.has(cp):
-		return chunks[cp]
-	var x0: int = cp.x * CHUNK_SIZE
-	var y0: int = cp.y * CHUNK_SIZE
+func _get_or_create_chunk(chunk_index: Vector2i) -> Chunk:
+	if chunks.has(chunk_index):
+		return chunks[chunk_index]
+	print("Creating chunk: " + str(chunk_index))
+	var x0: int = chunk_index.x * CHUNK_SIZE
+	var y0: int = chunk_index.y * CHUNK_SIZE
 	var generator: MapGenerator = MapGenerator.new()
 	var chunk: Chunk = Chunk.new()
+	print(" - generating bitmap...")
 	chunk.section = generator.generate_map(x0, y0, x0 + CHUNK_SIZE, y0 + CHUNK_SIZE)
-	chunk.chunk_index_x = cp.x
-	chunk.chunk_index_y = cp.y
-	chunk.global_pos_x = x0
-	chunk.global_pos_y = y0
+	chunk.chunk_index = chunk_index
+	chunk.global_pos = Vector2(x0, y0)
 	chunk.static_body_3d = StaticBody3D.new()
 	chunk.tiles = Node3D.new()
 	map_node.add_child(chunk.static_body_3d)
 	map_node.add_child(chunk.tiles)
-	chunk.static_body_3d.name = "Chunk_static_body_%d_%d" % [cp.x, cp.y]
-	chunk.tiles.name = "Chunk_%d_%d" % [cp.x, cp.y]
-	#_generate_common_collision_shapes(chunk)
-	chunks[cp] = chunk
-	return chunk
-
-func _generate_chunk(cp: Vector2i) -> void:
-	if chunks.has(cp):
-		return
-	var chunk: Chunk = _ensure_chunk_data(cp)	
-	
-	# Inspiration from generate_level
+	chunk.static_body_3d.name = "Chunk_static_body_%d_%d" % [chunk_index.x, chunk_index.y]
+	chunk.tiles.name = "Chunk_%d_%d" % [chunk_index.x, chunk_index.y]
+	chunks[chunk_index] = chunk
+	print(" - converting pixels to models...")
 	for y in range(chunk.section.y, chunk.section.y + chunk.section.h):
 		for x in range(chunk.section.x, chunk.section.x + chunk.section.w):
 			_add_tile(chunk.tiles, chunk.static_body_3d, chunk.section, x, y)
-	
+	print(" - done")
+	return chunk
 
-func _instantiate_model(graphic_parent: Node3D, collision_parent: StaticBody3D, model: Model, tile_pos: Vector3, angle: float):
+func _instantiate_model(graphic_parent: Node3D, collision_parent: StaticBody3D, model: Model, tile_index: Vector2i, angle: float):
+	var tile_pos: Vector3 = Vector3(float(tile_index.x) * TILE_SIZE, 0.0, float(tile_index.y) * TILE_SIZE)
 	var graphic_copy = model.graphic.duplicate()
 	graphic_copy.transform.origin = tile_pos
 	graphic_copy.rotate_y(angle)
 	graphic_parent.add_child(graphic_copy)
 	if(model.collision != null):
-		var collision_copy = model.collision.duplicate()
-		collision_copy.transform.origin = tile_pos
-		collision_copy.rotate_y(angle)
-		collision_parent.add_child(collision_copy)
+		var placed_tile: PlacedTile = PlacedTile.new()
+		placed_tile.model = model
+		placed_tile.angle = angle
+		placed_tiles[tile_index] = placed_tile
+		#var collision_shape = model.collision.duplicate()
+		#collision_shape.transform.origin = tile_pos
+		#collision_shape.rotate_y(angle)
+		#collision_parent.add_child(collision_shape)
 
 func _add_tile(parent: Node3D, static_body: StaticBody3D, section: MapGenerator.Section, x: int, y: int) -> void:
 	var tile_type: MapGenerator.TileType = section.get_tile(x, y)
-	var tile_pos: Vector3 = Vector3(float(x) * tile_size, 0.0, float(y) * tile_size)
+	var tile_index: Vector2i = Vector2i(x, y)
 	
 	# ALWAYS add a floor and ceiling
-	_instantiate_model(parent, static_body, model_floor, tile_pos, 0)
-	_instantiate_model(parent, static_body, model_ceiling, tile_pos, 0)
+	_instantiate_model(parent, static_body, model_floor, tile_index, 0)
+	_instantiate_model(parent, static_body, model_ceiling, tile_index, 0)
 
 	match tile_type:
 		MapGenerator.TileType.EMPTY:
 			return
+			
 		MapGenerator.TileType.CEILING_LIGHT:
-			var ceiling_light_inst: Node3D = ceiling_light_scene.instantiate()
-			parent.add_child(ceiling_light_inst)
-			ceiling_light_inst.transform.origin = tile_pos
-			#_move_collision_shapes(ceiling_light_inst, static_body)
+			_instantiate_model(parent, static_body, model_ceiling_light, tile_index, 0)
 			
 		MapGenerator.TileType.WALL:
 			# Use _get_tile_at for seamless transitions between chunks
@@ -158,95 +142,131 @@ func _add_tile(parent: Node3D, static_body: StaticBody3D, section: MapGenerator.
 			var wall_e: bool = _get_tile_at(x + 1, y) == MapGenerator.TileType.WALL
 			var wall_w: bool = _get_tile_at(x - 1, y) == MapGenerator.TileType.WALL
 			
-			if  ( wall_n &&  wall_s &&  wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_x, tile_pos, 0.0)
-			elif( wall_n &&  wall_s &&  wall_e && !wall_w): _place_wall(parent, static_body, model_wall_t, tile_pos, 0.0)
-			elif( wall_n &&  wall_s && !wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_t, tile_pos, PI)
-			elif( wall_n &&  wall_s && !wall_e && !wall_w): _place_wall(parent, static_body, model_wall_i, tile_pos, 0.0)
-			elif( wall_n && !wall_s &&  wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_t, tile_pos, -PI/2)
-			elif( wall_n && !wall_s &&  wall_e && !wall_w): _place_wall(parent, static_body, model_wall_l, tile_pos, -PI/2)
-			elif( wall_n && !wall_s && !wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_l, tile_pos, PI)
-			elif( wall_n && !wall_s && !wall_e && !wall_w): _place_wall(parent, static_body, model_wall_e, tile_pos, PI)
-			elif(!wall_n &&  wall_s &&  wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_t, tile_pos, PI/2)
-			elif(!wall_n &&  wall_s &&  wall_e && !wall_w): _place_wall(parent, static_body, model_wall_l, tile_pos, 0.0)
-			elif(!wall_n &&  wall_s && !wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_l, tile_pos, PI/2)
-			elif(!wall_n &&  wall_s && !wall_e && !wall_w): _place_wall(parent, static_body, model_wall_e, tile_pos, 0.0)
-			elif(!wall_n && !wall_s &&  wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_i, tile_pos, PI/2)
-			elif(!wall_n && !wall_s &&  wall_e && !wall_w): _place_wall(parent, static_body, model_wall_e, tile_pos, -PI/2)
-			elif(!wall_n && !wall_s && !wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_e, tile_pos, PI/2)
-			elif(!wall_n && !wall_s && !wall_e && !wall_w): _place_wall(parent, static_body, model_wall_x, tile_pos, 0.0)
+			if  ( wall_n &&  wall_s &&  wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_x, tile_index, 0.0)
+			elif( wall_n &&  wall_s &&  wall_e && !wall_w): _place_wall(parent, static_body, model_wall_t, tile_index, 0.0)
+			elif( wall_n &&  wall_s && !wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_t, tile_index, PI)
+			elif( wall_n &&  wall_s && !wall_e && !wall_w): _place_wall(parent, static_body, model_wall_i, tile_index, 0.0)
+			elif( wall_n && !wall_s &&  wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_t, tile_index, -PI/2)
+			elif( wall_n && !wall_s &&  wall_e && !wall_w): _place_wall(parent, static_body, model_wall_l, tile_index, -PI/2)
+			elif( wall_n && !wall_s && !wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_l, tile_index, PI)
+			elif( wall_n && !wall_s && !wall_e && !wall_w): _place_wall(parent, static_body, model_wall_e, tile_index, PI)
+			elif(!wall_n &&  wall_s &&  wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_t, tile_index, PI/2)
+			elif(!wall_n &&  wall_s &&  wall_e && !wall_w): _place_wall(parent, static_body, model_wall_l, tile_index, 0.0)
+			elif(!wall_n &&  wall_s && !wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_l, tile_index, PI/2)
+			elif(!wall_n &&  wall_s && !wall_e && !wall_w): _place_wall(parent, static_body, model_wall_e, tile_index, 0.0)
+			elif(!wall_n && !wall_s &&  wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_i, tile_index, PI/2)
+			elif(!wall_n && !wall_s &&  wall_e && !wall_w): _place_wall(parent, static_body, model_wall_e, tile_index, -PI/2)
+			elif(!wall_n && !wall_s && !wall_e &&  wall_w): _place_wall(parent, static_body, model_wall_e, tile_index, PI/2)
+			elif(!wall_n && !wall_s && !wall_e && !wall_w): _place_wall(parent, static_body, model_wall_x, tile_index, 0.0)
 
 #
 func _find_spawn_point() -> Vector2i:
-	var first_chunk: Chunk = _ensure_chunk_data(Vector2i(0, 0))
+	var first_chunk: Chunk = _get_or_create_chunk(Vector2i(0, 0))
 	for y in range(first_chunk.section.y, first_chunk.section.y + first_chunk.section.h):
 		for x in range(first_chunk.section.x, first_chunk.section.x + first_chunk.section.w):
 			if first_chunk.section.get_tile(x, y) != MapGenerator.TileType.WALL:
 				return Vector2i(x, y)
 	return Vector2i(1, 1)
 
+func _remove_chunk(chunk_index: Vector2i) -> void:
+	print("Removing chunk: " + str(chunk_index))
+	var chunk_node: Node3D = chunks[chunk_index].tiles
+	var chunk_collisions: StaticBody3D = chunks[chunk_index].static_body_3d
+	var chunk_name: String = chunk_node.name
+	var chunk_collisions_name: String = chunk_collisions.name
+	map_node.remove_child(chunk_node)
+	map_node.remove_child(chunk_collisions)
+	chunk_node.queue_free()
+	
+	# Clean up collision shapes that were moved to the global StaticBody3D ( not tested ) 
+	get_tree().call_group("col_" + chunk_name, "queue_free")
+	get_tree().call_group("col_" + chunk_collisions_name, "queue_free")
+	chunks.erase(chunk_index)
+
+var _last_tiles_where_collision_is_needed: Dictionary # Vector2i, bool   ( bool not used. treat as std::set )
+
+func _handle_static_collision_shapes() -> void:
+	var player_pos_3d: Vector3 = $Player/CharacterBody3D.transform.origin
+	var player_pos: Vector2 = Vector2(player_pos_3d.x, player_pos_3d.z)
+	var tile_index_of_player = Vector2i(int(player_pos.x), int(player_pos.y))
+	print("player tile index: " + str(tile_index_of_player))
+	
+	const COLLISION_RADIUS: int = 2
+
+	var x0 = tile_index_of_player.x - COLLISION_RADIUS
+	var x1 = tile_index_of_player.x + COLLISION_RADIUS
+	var y0 = tile_index_of_player.y - COLLISION_RADIUS
+	var y1 = tile_index_of_player.y + COLLISION_RADIUS
+	
+	var tiles_where_collision_is_needed: Array[Vector2i] = []
+	var tiles_where_collision_is_no_longer_needed = _last_tiles_where_collision_is_needed.duplicate()
+	
+	for y in range(y0, y1):
+		for x in range(x0, x1):
+			var tile_index = Vector2i(x, y)
+			tiles_where_collision_is_needed.append(tile_index)
+			tiles_where_collision_is_no_longer_needed.erase(tile_index)
+
+	# remove collision tile first
+	#for tile_index: Vector2i in tiles_where_collision_is_no_longer_needed.keys():
+	#	var chunk_index = _get_chunk_index(tile_index)
+	#	if chunks.has(chunk_index) && placed_tiles.has(tile_index):
+	#		var placed_tile: PlacedTile = placed_tiles[tile_index]
+	#		var chunk: Chunk = chunks[chunk_index]
+	#		chunk.static_body_3d.remove_child(placed_tile.collision_shape)
+	#		get_tree().call_group("col_" + placed_tile.collision_shape.name, "queue_free") # not sure if this is needed.
+			#print("collision [" + str(tile_index) + "] removed")
+	
+	_last_tiles_where_collision_is_needed = {}
+	for tile_index: Vector2i in tiles_where_collision_is_needed:
+		_last_tiles_where_collision_is_needed[tile_index] = true
+		if placed_tiles.has(tile_index):
+			var placed_tile: PlacedTile = placed_tiles[tile_index]
+			var chunk_index = _get_chunk_index(tile_index)
+			if placed_tile.collision_shape == null && chunks.has(chunk_index):
+				var chunk: Chunk = chunks[chunk_index]
+				var tile_pos: Vector3 = Vector3(float(tile_index.x) * TILE_SIZE, 0.0, float(tile_index.y) * TILE_SIZE)
+				placed_tile.collision_shape = placed_tile.model.collision.duplicate()
+				placed_tile.collision_shape.transform.origin = tile_pos
+				placed_tile.collision_shape.rotate_y(placed_tile.angle)
+				chunk.static_body_3d.add_child(placed_tile.collision_shape)
+				#print("collision [" + str(tile_index) + "] added")
+
+
 func _handle_world_generation() -> void:
-	var player_pos: Vector3 = $Player.transform.origin
-	var px: float = player_pos.x / tile_size
-	var py: float = player_pos.z / tile_size
+	var player_pos_3d: Vector3 = $Player/CharacterBody3D.transform.origin
+	var player_pos: Vector2 = Vector2(player_pos_3d.x, player_pos_3d.z)
+	#print("Player pos: " + str(player_pos))
 	
 	# Removal logic
 	var chunks_to_remove: Array[Vector2i] = []
-	for cp: Vector2i in chunks.keys():
-		var chunk_min_x: float = float(cp.x * CHUNK_SIZE)
-		var chunk_max_x: float = float((cp.x + 1) * CHUNK_SIZE)
-		var chunk_min_y: float = float(cp.y * CHUNK_SIZE)
-		var chunk_max_y: float = float((cp.y + 1) * CHUNK_SIZE)
-		
-		var closest_x: float = clamp(px, chunk_min_x, chunk_max_x)
-		var closest_y: float = clamp(py, chunk_min_y, chunk_max_y)
-		
-		var dist: float = Vector2(px - closest_x, py - closest_y).length()
-		if dist > REMOVAL_THRESHOLD:
-			chunks_to_remove.append(cp)
+	for chunk: Chunk in chunks.values():
+		var dist: float = (_get_center_chunk_pos(chunk.chunk_index) - player_pos).length()
+		if (dist - CHUNK_SIZE/2.0) > REMOVAL_THRESHOLD:
+			chunks_to_remove.append(chunk.chunk_index)
 			
-	for cp: Vector2i in chunks_to_remove:
-		var chunk_node: Node3D = chunks[cp].tiles
-		var chunk_collisions: StaticBody3D = chunks[cp].static_body_3d
-		var chunk_name: String = chunk_node.name
-		var chunk_collisions_name: String = chunk_collisions.name
-		map_node.remove_child(chunk_node)
-		map_node.remove_child(chunk_collisions)
-		chunk_node.queue_free()
-		
-		# Clean up collision shapes that were moved to the global StaticBody3D
-		get_tree().call_group("col_" + chunk_name, "queue_free")
-		get_tree().call_group("col_" + chunk_collisions_name, "queue_free")
-		
-		chunks.erase(cp)
+	for chunk_index: Vector2i in chunks_to_remove:
+		_remove_chunk(chunk_index)
 
-	var current_chunk_x: int = int(floor(px / float(CHUNK_SIZE)))
-	var current_chunk_y: int = int(floor(py / float(CHUNK_SIZE)))
+	var current_chunk_x: int = int(floor(player_pos.x / float(CHUNK_SIZE)))
+	var current_chunk_y: int = int(floor(player_pos.y / float(CHUNK_SIZE)))
 	
 	for dx in range(-1, 2):
 		for dy in range(-1, 2):
-			var cp: Vector2i = Vector2i(current_chunk_x + dx, current_chunk_y + dy)
-			if not chunks.has(cp):
-				var chunk_min_x: float = float(cp.x * CHUNK_SIZE)
-				var chunk_max_x: float = float((cp.x + 1) * CHUNK_SIZE)
-				var chunk_min_y: float = float(cp.y * CHUNK_SIZE)
-				var chunk_max_y: float = float((cp.y + 1) * CHUNK_SIZE)
-				
-				var closest_x: float = clamp(px, chunk_min_x, chunk_max_x)
-				var closest_y: float = clamp(py, chunk_min_y, chunk_max_y)
-				
-				var dist: float = Vector2(px - closest_x, py - closest_y).length()
-				if dist < GENERATION_THRESHOLD:
-					_generate_chunk(cp)
+			var chunk_index: Vector2i = Vector2i(current_chunk_x + dx, current_chunk_y + dy)
+			if not chunks.has(chunk_index):
+				var dist: float = (_get_center_chunk_pos(chunk_index) - player_pos).length()
+				print("chunk: [" + str(chunk_index) + "] check distance: " + str(dist - CHUNK_SIZE/2.0) + ")")
+				if (dist - CHUNK_SIZE/2.0) < GENERATION_THRESHOLD:
+					_get_or_create_chunk(chunk_index)
 
+func _get_chunk_index(tile_index: Vector2i) -> Vector2i:
+	return Vector2i(tile_index.x / CHUNK_SIZE, tile_index.y / CHUNK_SIZE)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
-	#_handle_world_generation()
-
-func generate_level() -> void:
-	# Keep for inspiration/compatibility but actual work is in _generate_chunk
-	_generate_chunk(Vector2i(0, 0))
+	_handle_world_generation()
+	_handle_static_collision_shapes()
 
 func _get_collision_shape(resource: Resource) -> CollisionShape3D:
 	var inst: Node3D = resource.instantiate()
@@ -270,3 +290,6 @@ func _load_model(tile: Resource) -> Model:
 	retval.graphic = _instantiate_and_remove_collision(tile)
 	retval.collision = _get_collision_shape(tile)
 	return retval
+
+func _get_center_chunk_pos(chunk_index: Vector2i) -> Vector2:
+	return Vector2((float(chunk_index.x * CHUNK_SIZE)) + CHUNK_SIZE/2.0, (float(chunk_index.y * CHUNK_SIZE)) + CHUNK_SIZE / 2.0)
